726336215d87267dc67d089698edbcb3a827cc4d
Andi Surya <andi502.as@gmail.com>
2021-07-28 16:47:56 +0700

    Finishing about_screen.dart

--- lib/about_screen.dart
+++ lib/about_screen.dart
@@ -23,6 +23,35 @@ class AboutScreenPage extends StatelessWidget {
           style: TextStyle(fontSize: 18, color: Colors.black),
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Bike Shop',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(top: 8),
+                      child: Text('Develop by : Andi Surya')),
+                  Container(
+                    margin: EdgeInsets.only(top: 16),
+                    child: Text(
+                      'This app was created to complete Dicoding \' Belajar Membuat Aplkasi Flutter untuk Pemula\' courses',
+                      textAlign: TextAlign.center,
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
